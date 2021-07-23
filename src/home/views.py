1beba1ba4de886b6b49803105b87dfef6ad1d064
shoko-inori <frank.gong.1006@hotmail.com>
2021-07-23 17:22:43 -0400

    Implement upload new subscription to database

--- src/home/views.py
+++ src/home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .forms import SubscriptionForm
+from .models import Subscription
 
 
 # Create your views here.
@@ -9,6 +10,15 @@ def home_view(request):
 
 def subscribe_view(request):
     subscription_form = SubscriptionForm()
+    if request.method == "POST":
+        subscription_form = SubscriptionForm(request.POST)
+        if subscription_form.is_valid():
+            ticker, email = \
+                subscription_form.cleaned_data["ticker"], subscription_form.cleaned_data["email"]
+            sub = Subscription(ticker=ticker, email=email)
+            sub.save()
+            request.user.subscription.add(sub)
+
     context = {
         "subscription_form": subscription_form
     }
