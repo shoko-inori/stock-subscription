2ab09cfbb997bcda907a8febd3ac93be8f5a709b
shoko-inori <frank.gong.1006@hotmail.com>
2021-07-23 18:06:45 -0400

    Implement ticker validation check

--- src/home/forms.py
+++ src/home/forms.py
@@ -1,6 +1,14 @@
 from django import forms
+import yfinance as yf
 
 
 class SubscriptionForm(forms.Form):
     ticker = forms.CharField(max_length=5)
     email = forms.EmailField()
+
+    def clean_ticker(self):
+        ticker = self.cleaned_data.get("ticker")
+        if yf.Ticker(ticker).info["regularMarketPrice"]:
+            return ticker
+        else:
+            raise forms.ValidationError("This ticker is not valid. Please try another ticker. ")
