0c5c9328aff734717668a03aa78bd6f91e2e515c
shoko-inori <frank.gong.1006@hotmail.com>
2021-07-23 03:38:40 -0400

    Implement the view for user account registration

--- src/register/views.py
+++ src/register/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+def register_view(response):
+    register_form = UserCreationForm()
+    if response.method == "POST":
+        register_form = UserCreationForm(response.POST)
+        if register_form.is_valid():
+            register_form.save()
+        else:
+            register_form = UserCreationForm()
+    return render(response, "register.html", {"register_form": register_form})
