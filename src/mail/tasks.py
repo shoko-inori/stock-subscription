08958d87b4618f3aa19c8592d2cfc1213d3a755e
shoko-inori <frank.gong.1006@hotmail.com>
2021-07-25 06:09:09 -0400

    Move the API call out of the actual function to send emails, enabling optimization in the schedule task

--- src/mail/tasks.py
+++ src/mail/tasks.py
@@ -1,27 +1,22 @@
 from celery import shared_task
+from stocksubscription.celery import app
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
 from django.utils.html import strip_tags
+from home.models import Subscription
 import yfinance as yf
-# from time import sleep
 
 
 @shared_task
 def send_email_task(mailto, tickers):
-    # Call API to obtain latest prices of all tickers
-    ticker_string = ""
-    for sub in tickers:
-        ticker_string = "{} {}".format(ticker_string, sub.ticker)
-    yf_response = yf.Tickers(ticker_string.strip())
-
     # Compose the context to feed the template
     context = {
         "subscriptions": [],
     }
     for sub in tickers:
         context["subscriptions"].append({
-            "ticker": sub.ticker,
-            "current_price": yf_response.tickers[sub.ticker].info["regularMarketPrice"],
+            "ticker": sub,
+            "current_price": tickers[sub],
         })
 
     # Send the email
